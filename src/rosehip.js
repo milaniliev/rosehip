1e5e5c7bb58aed2c30aef07b3e648ffb1ac81b27
Milan Iliev <milaniliev@gmail.com>
2015-08-24 16:46:53 -0400

    Fresh new look for fall!

--- src/rosehip.js
+++ src/rosehip.js
@@ -51,7 +51,7 @@ module.exports = {
     success (options) {
       let indicator = document.createElement('test_indicator')
       indicator.classList.add('success')
-      indicator.innerHTML = `<status>PASS</status> <test_name>${options.name}</test_name>`
+      indicator.innerHTML = `<status>P A S S</status><test_name>${options.name}</test_name>`
 
       this.element.appendChild(indicator)
     }
@@ -59,7 +59,7 @@ module.exports = {
     failure (options){
       let indicator = document.createElement('test_indicator')
       indicator.classList.add('failure')
-      indicator.innerHTML = `<status>FAIL</status> <test_name>${options.name}</test_name>
+      indicator.innerHTML = `<status>F A I L</status> <test_name>${options.name}</test_name>
         <stack_trace>${options.error.stack.replace(/\n/g,'<br/>')}</stack_trace>
       `
 
